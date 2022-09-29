b78b1defc60a0505e9ff09aa72298cf43bfebb68
ximeen <gabriximenes100@gmail.com>
2022-09-29 20:30:21 -0300

    received front end data

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
+import {useEffect, useState} from 'react'
 import { GameBanner } from './componets/GameBanner'
 import Logo from './assets/logo.svg'
 import { CreateGameAd } from './componets/CreateGameAd'
 
+interface Game{
+    id: string;
+    title: string;
+    banner: string;
+    _count: {
+        ads: number
+    }
+}
+
 export function App(){
-    return(
+    const [games, setGames] = useState<Game[]>([])
+
+    useEffect(()=>{
+        fetch('http://localhost:3000/games')
+            .then(res => res.json())
+            .then(data => {
+                setGames(data)
+            })
+    },[])
+
+    return( 
         <div className='flex items-center max-w-[1344px] mx-auto my-20 flex-col'>
             <img src={Logo} alt="Logo da aplicação nlw eSport" />
 
@@ -11,18 +31,19 @@ export function App(){
                  Seu <span className='bg-duo-gradiant bg-clip-text text-transparent'>duo</span> está aqui 
             </h1>
             <div className='grid grid-cols-6 gap-6 mt-16'>
-                <GameBanner
-                    banner_url='/game-1.png'
-                    ads_count={5}
-                    title="League of legends"
-                />
-                <GameBanner
-                    banner_url='/image2.png'
-                    ads_count={1}
-                    title="Dota 2"
-                />
-            
+                {games.map((game) => {
+                    return(
+                        <GameBanner
+                            key={game.id}
+                            title={game.title}
+                            banner_url={game.banner}
+                            ads_count={game._count.ads}
+
+                        />
+                    )
+                })}
             </div>
+
             <CreateGameAd/>
         </div>
         
